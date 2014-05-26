8f07db3b2163a0a39a095e5ff62ff345ea087c4e
Anatoly Svichkarev <tolik0393@mail.ru>
2014-05-26 21:24:48 +0700

    implement simple ListView Activity

--- src/ru/svichkarev/metcast/MyActivity.java
+++ src/ru/svichkarev/metcast/MyActivity.java
@@ -2,14 +2,36 @@ package ru.svichkarev.metcast;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MyActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
+    // TODO: для тестирования
+    String[] countries = { "Россия", "США", "Великобритания", "Франция", "Китай",
+            "Россия", "США", "Великобритания", "Франция", "Китай"};
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, countries);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // TODO: для тестирования
+                Toast.makeText( getApplicationContext(),
+                        "You choose " + adapterView.getItemAtPosition(i).toString(),
+                        Toast.LENGTH_LONG ).show();
+            }
+        });
     }
 }
